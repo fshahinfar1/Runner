191ef9a07e378464be5a351753416b67f29f2ab1
fshahinfar1 <fsh1.dev@gmail.com>
2018-12-08 20:32:33 +0330

    Add simple player controller

---
+++ Assets/Scripts/Player/Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Player
+{
+    [RequireComponent(typeof(PlayerPhysicsConfig))]
+    public class Controller : MonoBehaviour
+    {
+
+        private new Rigidbody rigidbody;
+        private Vector3 velocity;
+
+        private float maxPartialSpeed = 3;
+
+        private void Awake()
+        {
+            rigidbody = transform.GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+
+            float verticalNormal = Input.GetAxis("Vertical");
+            float horizontalNormal = Input.GetAxis("Horizontal");
+
+            velocity = rigidbody.velocity;
+            velocity.z = maxPartialSpeed * verticalNormal;
+            velocity.x = maxPartialSpeed * horizontalNormal;
+            rigidbody.velocity = velocity;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerPhysicsConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerPhysicsConfig : MonoBehaviour
+    {
+
+        private new Rigidbody rigidbody;
+
+
+        private void Awake()
+        {
+            rigidbody = transform.GetComponent<Rigidbody>();
+            rigidbody.mass = 10;
+            rigidbody.useGravity = true;
+            rigidbody.drag = 0.1f;
+
+            rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        }
+    }
+}
