71730d60032bee536f5048cb7f78e6abb52e5680
fshahinfar1 <fsh1.dev@gmail.com>
2018-12-08 21:05:24 +0330

    add camera follower script

--- Assets/Scripts/Camera/CameraFollower.cs
+++ Assets/Scripts/Camera/CameraFollower.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class CameraFollower : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject followed;
+
+    public float distance;
+    public float height;
+
+    private Vector3 position;
+
+    private void Update()
+    {
+        position = followed.transform.position;
+        // keep the height
+        position.y += height;
+        position.z -= distance;
+        transform.position = position;
+    }
 }
