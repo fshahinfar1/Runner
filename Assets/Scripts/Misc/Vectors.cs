00a264fab49fc3f9c3dc848c2efe47511905c870
fshahinfar1 <fsh1.dev@gmail.com>
2019-01-24 23:16:52 +0330

    Add some vector operations

--- Assets/Scripts/Misc/Vectors.cs
+++ Assets/Scripts/Misc/Vectors.cs
@@ -11,9 +11,37 @@ public class Vectors
             throw new System.Exception("dot: two vector lenght should be equal!");
         }
         float result = 0;
-        for (int i=0; i<a.Length; i++)
+        for (int i = 0; i < a.Length; i++)
         {
             result += a[i] * b[i];
+        }
+        return result;
+    }
+
+    public static float[] ElementWiseProduct(float[] a, float[] b)
+    {
+        if (a.Length != b.Length)
+        {
+            throw new System.Exception("dot: two vector lenght should be equal!");
+        }
+        float[] result = new float[a.Length];
+        for (int i = 0; i < a.Length; i++)
+        {
+            result[i] = a[i] * b[i];
+        }
+        return result;
+    }
+
+    public static float[] Add(float[] a, float[] b)
+    {
+        if (a.Length != b.Length)
+        {
+            throw new System.Exception("dot: two vector lenght should be equal!");
+        }
+        float[] result = new float[a.Length];
+        for (int i = 0; i < a.Length; i++)
+        {
+            result[i] = a[i] + b[i];
         }
         return result;
     }
